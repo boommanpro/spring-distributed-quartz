f00fd5aabc39f25739790218ca09734020c2422e
boommanpro <boommanpro@gmail.com>
2020-12-09 20:54:20 +0800

    build(vue build dist.zip):

--- front/spring-distributed-quartz-front/vue.config.js
+++ front/spring-distributed-quartz-front/vue.config.js
@@ -1,26 +1,43 @@
-const path =require('path')
+const path = require('path')
 
-function resolve(dir){
-  return path.join(__dirname,dir) //path.join(__dirname)设置为绝对路径
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+
+function resolve (dir) {
+  return path.join(__dirname, dir) //path.join(__dirname)设置为绝对路径
 }
 
 //vue.config.js 配置
-module.exports={
+module.exports = {
   runtimeCompiler: true,
   publicPath: '/', // 设置打包文件相对路径
-  configureWebpack:{
-    devtool: 'source-map'
+  configureWebpack: {
+    devtool: 'source-map',
+    plugins: [
+      // help => https://www.npmjs.com/package/filemanager-webpack-plugin
+      new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
+        events: {
+          onEnd: {
+            delete: [   //首先需要删除项目根目录下的dist.zip
+              './dist.zip',
+            ],
+            archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+              { source: './dist', destination: './dist.zip' },
+            ]
+          }
+        }
+      })
+    ],
   },
-  chainWebpack:(config)=>{
+  chainWebpack: (config) => {
     config.resolve.alias
-    .set('@',resolve('src'))
-    .set('components',resolve('src/components'))
-    .set('views',resolve('src/views'))
-    .set('assets',resolve('src/assets'))
-    .set('page',resolve('src/page'))
+      .set('@', resolve('src'))
+      .set('components', resolve('src/components'))
+      .set('views', resolve('src/views'))
+      .set('assets', resolve('src/assets'))
+      .set('page', resolve('src/page'))
   },
-  devServer:{
-    host:'localhost',
+  devServer: {
+    host: 'localhost',
     port: 8222,
     proxy: {
       '/api': {
@@ -32,5 +49,5 @@ module.exports={
         }
       }
     }
-  }
+  },
 }
